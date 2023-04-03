3907c4e14a998ed608f4aa5b6840140965e14361
dawnzzz <635441876@qq.com>
2023-04-03 21:45:12 +0800

    add logger

---
+++ logger/logger.go
@@ -0,0 +1,45 @@
+package logger
+
+import "github.com/sirupsen/logrus"
+
+var logger = logrus.New()
+
+func Debug(args ...interface{}) {
+	logger.Info(args...)
+}
+
+func Info(args ...interface{}) {
+	logger.Info(args...)
+}
+
+func Warn(args ...interface{}) {
+	logger.Info(args...)
+}
+
+func Error(args ...interface{}) {
+	logger.Error(args...)
+}
+
+func Fatal(args ...interface{}) {
+	logger.Fatal(args...)
+}
+
+func Debugf(format string, args ...interface{}) {
+	logger.Debugf(format, args...)
+}
+
+func Infof(format string, args ...interface{}) {
+	logger.Infof(format, args...)
+}
+
+func Warnf(format string, args ...interface{}) {
+	logger.Warnf(format, args...)
+}
+
+func Errorf(format string, args ...interface{}) {
+	logger.Errorf(format, args...)
+}
+
+func Fatalf(format string, args ...interface{}) {
+	logger.Fatalf(format, args...)
+}
